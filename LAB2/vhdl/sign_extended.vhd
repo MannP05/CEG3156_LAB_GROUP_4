c5ba45291e5f22757a2eccb6cd7dd4b3b9f53870
MannP05 <mannapatel06@gmail.com>
2026-03-16 23:06:12 -0400

    maybe idk works cool im tired

--- LAB2/vhdl/sign_extended.vhd
+++ LAB2/vhdl/sign_extended.vhd
@@ -3,20 +3,15 @@ use ieee.std_logic_1164.all;
 
 entity sign_extended is
     port(
-        input : in std_logic_vector(15 downto 0);
-        output : out std_logic_vector(31 downto 0)
+        i  : in  std_logic_vector(15 downto 0);
+        o : out std_logic_vector(7 downto 0)
     );
 end sign_extended;
 
 architecture structural of sign_extended is
 begin
-    -- original 16 bits: passvthrough the bits from input
-    original: for i in 0 to 15 generate
-        output(i) <= input(i);
-    end generate;
-
-    -- Upper 16 bits: repeat the sign bit 
-    upper: for i in 16 to 31 generate
-        output(i) <= input(15);
+    -- Just pass through the lower 8 bits
+    pass: for j in 0 to 7 generate
+        o(j) <= i(15);  -- sign extend
     end generate;
 end structural;
