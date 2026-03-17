569ce8497d1354f8238709a30be4031f135006bd
Surya Vasudev <suryavasudev05@gmail.com>
2026-03-16 21:04:00 -0400

    big refactor

--- LAB2/vhdl/en_nBitMux2to1.vhd
+++ LAB2/vhdl/en_nBitMux2to1.vhd
@@ -6,8 +6,8 @@ ENTITY en_nBitMux2to1 IS
     PORT(
         i_en        : IN  STD_LOGIC;
         i_sel       : IN  STD_LOGIC;
-        i_d0, i_d1  : IN  STD_LOGIC_VECTOR(n-1 DOWNTO 0);
-        o_q         : OUT STD_LOGIC_VECTOR(n-1 DOWNTO 0)
+        i_0, i_1  : IN  STD_LOGIC_VECTOR(n-1 DOWNTO 0);
+        out         : OUT STD_LOGIC_VECTOR(n-1 DOWNTO 0)
     );
 END en_nBitMux2to1;
 
@@ -26,14 +26,14 @@ BEGIN
     gen_mux: FOR i IN 0 TO n-1 GENERATE
         mux_i: mux2to1 PORT MAP(
             s  => i_sel,
-            x0 => i_d0(i),
-            x1 => i_d1(i),
+            x0 => i_0(i),
+            x1 => i_1(i),
             y  => int_mux_out(i)
         );
     END GENERATE;
 
     gen_en: FOR i IN 0 TO n-1 GENERATE
-        o_q(i) <= int_mux_out(i) AND i_en;
+        out(i) <= int_mux_out(i) AND i_en;
     END GENERATE;
 
 END structural;
